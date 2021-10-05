ab9275b0ae11dadb7989f759d0e5143112031c56
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2021-10-05 02:46:29 -0400

    I'm honestly shocked I got anything done today

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a machine learning model for Magic: the Gathering, which decides whether
 
 Of note, the dataset only shows the cards in the final opening hand, after resolving all mulligans and putting cards on bottom of library. This leads to some bias. For instance, there are very few one-land hands in the data set, because players generally mulligan one-land hands. The only one-land hands in the dataset are hands that the player chose to keep, which means they likely have a disproportionately high number of early plays/cheap draw spells/mana fixing. Therefore, the model likely underestimates the extent to which only having one land hurts your chances of winning.
 
-This also leads to a complexity of interpretation with regard to hands of less than seven cards. On a mulligan to six, for instance, the model predicts the probability of winning based on the six cards ultimately kept, but the player sees seven cards when deciding whether to mulligan to five, before putting one on bottom of library. A future function will be implemented to predict the probability of winning for each six-card combination of the seven cards, and the user will be advised to choose the combination with the highest chance of winning.
+This also leads to a complexity of interpretation with regard to hands of less than seven cards. On a mulligan to six, for instance, the model predicts the probability of winning based on the six cards ultimately kept, but the player sees seven cards when deciding whether to mulligan to five, before putting one on bottom of library. This does not affect training or testing, but at prediction time, the prediction function predicts the probability of winning for each six-card combination of the seven cards, and the user is advised to choose the combination with the highest chance of winning.
 
 This repository also includes a logistic regression model, which makes significantly worse predictions. It was implemented mostly as a quick and dirty way to get TensorFlow configured and working, but it also serves to establish a linear baseline for predictive accuracy to compare more complex models against.
 
@@ -14,8 +14,6 @@ Some functions also use MTGJSON set files, which are available at https://mtgjso
 ## Near term things for me to do
 
     Make the model run server side
-
-    Write the card bottoming function
 
     Set up a website to host this on
 
--- prediction.py
+++ prediction.py
@@ -5,6 +5,7 @@ from tensorflow.keras import layers
 from sklearn.calibration import calibration_curve
 import matplotlib.pyplot as plt
 from preprocessing import getMulliganWinRates
+from itertools import combinations
 
 def showExamples(num, data, preds, label):#only configured for neural-preprocessed data
     for i in range(num):
@@ -36,19 +37,50 @@ def getWeights(model):#Only works for the logistic regression model, with featur
         print(str(cols[index]["config"]["key"])+": "+str(weights[index]))
     print("Bias: "+str(bias))
 
-def enterExample(model, data):#configured for neural network, should reconfigure for features parameter instead of data
-    features = {name: np.array([0.0]) for name, value in data.items()}
-    features.pop("Unnamed: 0")
-    features.pop("won")
-    cards = float(input("Number of cards"))
-    onplay = float(input("On play? 1 for yes, 0 for no."))
-    features["cards"][0]=cards/7
-    features["on_play"][0]=onplay
-    for i in range(int(cards)):
-        card = input("Enter card name").replace(" ", "_").replace(",", "").replace("'", "")
-        features["opening_hand_"+card][0]+=1/7
-    preds = model.predict(x=features, verbose=1)
-    print(preds[0])
+def enterExample():
+    hand = [float(input("Number of cards")), float(input("On play? 1 for yes, 0 for no."))]
+    count = int(input("Number of cards to enter"))
+    for i in range(count):
+        hand.append(input("Enter card name"))
+    return hand
+
+def predictExample(example, model, features):#configured for neural network, should reconfigure for features parameter instead of data
+    if(len(example)==example[0]+2):
+        features = {name: np.array([0.0]) for name in features.keys()}
+        features["cards"][0]=example[0]/7
+        features["on_play"][0]=example[1]
+        for card in example[2:]:
+            features["opening_hand_"+card.replace(" ", "_").replace(",", "").replace("'", "")][0]+=1/7
+        preds = model.predict(x=features, verbose=1)
+        print("Probability of winning: "+str(preds[0]))
+    
+    elif(len(example)>example[0]+2):#this could also do what the above does, but it's a little slower
+        possibleHands = list(combinations(range(2, len(example)), int(example[0])))
+        features = {name: np.full(shape=len(possibleHands), fill_value=0.0, dtype=np.float) for name in features.keys()}
+        for i in range(len(possibleHands)):
+            features["cards"][i]=example[0]/7
+            features["on_play"][i]=example[1]
+            indexes=list(possibleHands[i])
+            for index in indexes:
+                card=example[index]
+                features["opening_hand_"+card.replace(" ", "_").replace(",", "").replace("'", "")][i]+=1/7
+        preds = model.predict(x=features, verbose=1)
+        for i in range(len(possibleHands)):#remove this for loop to only show the best hand
+            handIndices = list(possibleHands[i])
+            hand = []
+            for index in handIndices:
+                hand.append(example[index])
+            print("This hand: "+str(hand)+" has this probability of winning: "+str(preds[i]))
+        bestIndex = np.argmax(preds)
+        bestHandIndices = list(possibleHands[bestIndex])
+        bestHand = []
+        for index in bestHandIndices:
+            bestHand.append(example[index])
+        print("Keep the following cards: "+str(bestHand))
+        print("Probability of winning: "+str(preds[bestIndex]))
+    
+    else:
+        print("Something went seriously wrong here, the hand is missing some cards")
 
 
 model = tf.keras.models.load_model('model_30_sep_2021_2')
@@ -57,12 +89,12 @@ data = pd.read_csv("test_data.csv", header=0)
 features = {name: np.array(value) for name, value in data.items()}
 label = np.array(features.pop("won"))
 
-preds = model.predict(x=features, verbose=1)
-showExamples(5, data, preds, label)
-plotCalibrationCurve(preds, label)
-getAccuracy(preds, label)
+#preds = model.predict(x=features, verbose=1)
+#showExamples(5, data, preds, label)
+#plotCalibrationCurve(preds, label)
+#getAccuracy(preds, label)
 #model.evaluate(x=features, y=label, verbose=1)
 
 #data = pd.read_csv("training_data.csv", header=0)
 #getMulliganWinRates(data, 1)
-#enterExample(model, data)
+predictExample(enterExample(), model, features)
